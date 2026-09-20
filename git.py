da61d1827be7b5fda00f985302f8f10bd36dc397
Charlotte Wang <charlotte.wang3@education.nsw.gov.au>
2026-09-21 09:31:15 +1000

    first commit

---
+++ git.py
@@ -0,0 +1,8 @@
+import time
+
+# loop from 10 to 1 decreasing by 1
+for i in range(10, 0, -1):
+    print(i)
+    time.sleep(1)  # pause execution 1 sec
+
+print("Time's up!")
---
+++ git2.py
@@ -0,0 +1,1 @@
+https://github.com/charlottewang3/python.git
